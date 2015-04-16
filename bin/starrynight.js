b52f63661eb4f503159f7e438cc0e3d548268648
Adam Monsen <haircut@gmail.com>
2015-04-15 22:48:43 -0700

    toss maybe-unused spawn() callback
    These event listeners work for me. The spawn() callback didn't! Not sure
    if that had something to do with my Node.js version (v0.10.38 at the
    time of writing).

--- bin/starrynight.js
+++ bin/starrynight.js
@@ -539,20 +539,8 @@ function parseRunTestArguments(npmPrefix){
             }
 
             var nightwatchEnv = _.extend(process.env, {npm_config_prefix: npmPrefix});
-            var nightwatch = childProcess.spawn(nightwatchCommand, ['-c', configFileLocation], {env: nightwatchEnv}, function(error, result){
-              if(error){
-                // FIXME: I never see this, even when nightwatchCommand points to a nonexistant file
-                console.log("[StarryNight] ERROR spawning nightwatch: ", error);
-                //process.exit(1);
-              }
-              if(result){
-                // FIXME: Does this do anything? Or does the stdout.on() call (below) win/supersede?
-                console.log("result", result);
-              }
-            });
-
-
             var frameworkExitCode = 0;
+            var nightwatch = childProcess.spawn(nightwatchCommand, ['-c', configFileLocation], {env: nightwatchEnv});
             nightwatch.stdout.on('data', function(data){
               console.log(data.toString().trim());
 
@@ -560,6 +548,13 @@ function parseRunTestArguments(npmPrefix){
               if(data.toString().indexOf("✖") > -1){
                 frameworkExitCode = 1;
               }
+            });
+            nightwatch.stderr.on('data', function(data) {
+              console.error(data.toString());
+            });
+            nightwatch.on('error', function(error){
+              console.error('[StarryNight] ERROR spawning nightwatch. nightwatchCommand was', nightwatchCommand);
+              throw error;
             });
             nightwatch.on('close', function(nightwatchExitCode){
               if(nightwatchExitCode === 0){
