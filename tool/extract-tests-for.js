1a7e979233907dc00d9666cfc88573fabfb355ed
Abigail Watson <awatson1978@gmail.com>
2015-07-29 01:05:32 -0500

    had to revert writeConfigObj() because of timing issues

--- tool/extract-tests-for.js
+++ tool/extract-tests-for.js
@@ -1,37 +1,39 @@
 // so we can read files from the filesystem
-var filesystem = require('fs');
+var filesystem = require( 'fs' );
 
 // cheerio provides DOM/jQuery utilities to lets us parse html
-var cheerio = require('cheerio');
+var cheerio = require( 'cheerio' );
 
 
-module.exports = function(secondArgument){
+module.exports = function ( secondArgument ) {
   //console.log( "Extracting ids from " + secondArgument);
-  filesystem.readFile(secondArgument, {encoding: 'utf-8'}, function(error, data){
-    if(data){
+  filesystem.readFile( secondArgument, {
+    encoding: 'utf-8'
+  }, function ( error, data ) {
+    if ( data ) {
       //console.log(data.toString());
-      $ = cheerio.load(data.toString())
+      $ = cheerio.load( data.toString() );
       var ids = new Array();
-      $('[id]').each(function() { //Get elements that have an id=
-        ids.push($(this).attr("id")); //add id to array
-      });
+      $( '[id]' )
+        .each( function () { //Get elements that have an id=
+          ids.push( $( this )
+            .attr( 'id' ) ); //add id to array
+        } );
 
-      var fileText = "";
-      fileText += "exports.command = function () {\n";
-      fileText += "  this\n";
-      ids.forEach(function(id){
-        fileText += '    .verify.elementPresent("#' + id + '")\n';
-      });
-      fileText += ";";
-      fileText += "  return this;\n";
-      fileText += "};";
+      var fileText = '';
+      fileText += 'exports.command = function () {\n';
+      fileText += '  this';
+      ids.forEach( function ( id ) {
+        fileText += '\n    .verify.elementPresent("#' + id + '")';
+      } );
+      fileText += ';';
+      fileText += '  return this;\n';
+      fileText += '};';
 
-      console.log(fileText);
-
-      //console.log(ids);
+      console.log( fileText );
     }
-    if(error){
-      console.error(error);
+    if ( error ) {
+      console.error( error );
     }
-  });
-}
+  } );
+};
--- tool/generate-autoconfig.js
+++ tool/generate-autoconfig.js
@@ -15,21 +15,6 @@ const bunyan = require('bunyan');
 const log = bunyan.createLogger({name: 'starrynight'});
 log.level('warn');
 
-function writeConfigObj (autoConfigObject) {
-  log.debug('autoConfigObject', autoConfigObject);
-
-  // Write Our New Config File
-  fs.writeJson(
-    '.meteor/nightwatch.json',
-    autoConfigObject,
-    {spaces: 2},
-    function writing (error, result) {
-      if (error) { log.error(error); }
-      log.info('Writing .meteor/nightwatch.json');
-      log.debug('writeJson result was : ' + result);
-    }
-  );
-}
 
 // module.exports = function(secondArgument, thirdArgument, fourthArgument){
 module.exports = function generateNightWatchConfig (npmPrefix, options) {
@@ -40,7 +25,7 @@ module.exports = function generateNightWatchConfig (npmPrefix, options) {
 
   if (options) {
     if (options.trace) { log.level(options.trace); }
-//    log.error('No, not really an error but you supplied --trace=' + options.trace);
+
     // Read Our Config File Template
     fs.readJson(
       npmPrefix + '/lib/node_modules/starrynight/configs/nightwatch/autoconfig.json',
@@ -49,64 +34,60 @@ module.exports = function generateNightWatchConfig (npmPrefix, options) {
         if (options.mnml || options.minimal) {
           log.info('------------------------------------------');
           log.info('Removing "custom path"  elements     .... ');
+
           delete autoConfigObject.custom_commands_path;
           delete autoConfigObject.custom_assertions_path;
-          writeConfigObj(autoConfigObject);
+
+          fs.writeJson(
+            '.meteor/nightwatch.json',
+            autoConfigObject,
+            {spaces: 2},
+            function writing (error, result) {
+              if (error) { log.error(error); }
+              log.info('Writing .meteor/nightwatch.json');
+              log.debug('writeJson result was : ' + result);
+            }
+          );
+
         } else {
           log.info('------------------------------------------');
           log.info('Searching files for .test directories.... ');
+
           // Search The Filesystem
           // looking for .tests directories in the filesystem
           // which don't get picked up by the meteor bundler
           find.eachdir('.tests', options.root, function (testDir) {
-           // Update Our New Config Object
+
+            log.debug('testDir', testDir);
+
+            // Update Our New Config Object
             autoConfigObject.custom_commands_path.push(testDir);
             // Test For Subdirecotries
             find.eachdir('actions', testDir, function (actionsDir) {
               // Update Our New Config Object
               autoConfigObject.custom_commands_path.push(actionsDir);
             });
-          }).end( writeConfigObj(autoConfigObject) );
+
+          }).end( function () {
+            log.debug('autoConfigObject', autoConfigObject);
+
+            // Write Our New Config File
+            fs.writeJson(
+              '.meteor/nightwatch.json',
+              autoConfigObject,
+              {spaces: 2},
+              function writing (error, result) {
+                if (error) { log.error(error); }
+                log.info('Writing .meteor/nightwatch.json');
+                log.debug('writeJson result was : ' + result);
+              }
+            );
+          });
         }
 
-        log.info('Updated .meteor/nightwatch.json.');
+        //log.info('Updated .meteor/nightwatch.json.');
+        console.log('Updated .meteor/nightwatch.json.');
       }
     );
   }
 };
-
-
-/*
-// cheerio provides DOM/jQuery utilities to lets us parse html
-var cheerio = require('cheerio');
-
-
-module.exports = function(secondArgument){
-  //log.info( 'Extracting ids from ' + secondArgument);
-  filesystem.readFile(secondArgument, {encoding: 'utf-8'}, function(error, data){
-    if(data){
-      //log.info(data.toString());
-      $ = cheerio.load(data.toString())
-      var ids = new Array();
-      $('[id]').each(function() { //Get elements that have an id=
-        ids.push($(this).attr('id')); //add id to array
-      });
-
-      var fileText = '';
-      fileText += 'exports.command = function() {\n';
-      fileText += '  this\n';
-      ids.forEach(function(id){
-        fileText += '    .verify.elementPresent('#' + id + '')\n';
-      });
-      fileText += '  return this;\n';
-      fileText += '};';
-
-      log.info(fileText);
-
-      //log.info(ids);
-    }
-    if(error){
-      console.error(error);
-    }
-  });
-}*/
