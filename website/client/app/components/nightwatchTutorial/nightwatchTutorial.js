569732680684408a9bbc113250bf6f0cccfb4d03
Abigail Watson <awatson1978@gmail.com>
2016-08-31 22:44:49 -0500

    nightwatch tutorial

---
+++ website/client/app/components/nightwatchTutorial/nightwatchTutorial.js
@@ -0,0 +1,16 @@
+Router.route("/nightwatch", {
+  name:"nightwatchTutorial",
+  template:"nightwatchTutorial"
+});
+
+Template.nightwatchTutorial.helpers({
+  rendered: function(){
+
+  }
+});
+
+Template.nightwatchTutorial.events({
+  "click #elementId": function(event, template){
+
+  }
+});
